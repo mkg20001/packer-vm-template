6ef2796d10b384e81ee925cc541197768119a86a
Maciej Krüger <mkg20001@gmail.com>
2019-07-15 10:35:16 +0200

    fix: git native protocol being used by npm

--- modules/_root.sh
+++ modules/_root.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ $(id -u) -gt 0 ]; then
-  echo "FEHLER: Dieser Befehl muss als Benutzer root ausgeführt werden. Verwenden Sie bitte stattdessen 'sudo $CMD $*'" >&2
+  echo "FEHLER: Dieser Befehl muss als Benutzer root ausgeführt werden. Verwenden Sie bitte stattdessen 'sudo $CMD'" >&2
   exit 2
 fi
